83bb8b3ea40da8c9658a298eef78d024daa90d7b
Sean Wong <serotonincrash@gmail.com>
2026-03-12 19:17:09 +0200

    fix: clarity and typo in error message

--- Sources/FoliBusAPI/Client/Core/FoliClient.swift
+++ Sources/FoliBusAPI/Client/Core/FoliClient.swift
@@ -77,7 +77,7 @@ public actor FoliClient {
         do {
             self.cache = try Foli.DiskCache(timeout: timeout)
         } catch {
-            print("An error occurred initialising the cache for FoliAPI. Defaulting to no cache implementation.")
+            print("An error occurred initialising the cache for FoliBusAPI. Defaulting to no cache implementation.")
             self.cacheBehavior = .noCache
         }
     }
