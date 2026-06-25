e81a4972509915971fc727ea49a313183dac93ca
Sean Wong <serotonincrash@gmail.com>
2026-06-26 00:30:56 +0800

    fix(concurrency): make request functions in client nonisolated

--- Sources/FoliBusAPI/Client/Core/FoliClient+Requesting.swift
+++ Sources/FoliBusAPI/Client/Core/FoliClient+Requesting.swift
@@ -5,22 +5,22 @@ extension FoliClient {
     // MARK: - Request Forwarders
 
     /// Fetch and decode a response from a SIRI endpoint.
-    internal func requestSIRI<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
+    internal nonisolated func requestSIRI<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
         try await requester.requestSIRI(path, as: type, headers: headers)
     }
 
     /// Fetch and decode a response from a GTFS endpoint.
-    internal func requestGTFS<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
+    internal nonisolated func requestGTFS<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
         try await requester.requestGTFS(path, as: type, headers: headers)
     }
 
     /// Fetch and decode a response from an Alerts endpoint.
-    internal func requestAlerts<T: Decodable>(_ path: String, as type: T.Type = T.self) async throws -> T {
+    internal nonisolated func requestAlerts<T: Decodable>(_ path: String, as type: T.Type = T.self) async throws -> T {
         try await requester.requestAlerts(path, as: type)
     }
 
     /// Fetch and decode a response from a GeoJSON endpoint.
-    internal func requestGeoJSON<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
+    internal nonisolated func requestGeoJSON<T: Decodable>(_ path: String, as type: T.Type = T.self, headers: [String: String] = [:]) async throws -> T {
         try await requester.requestGeoJSON(path, as: type, headers: headers)
     }
 }
